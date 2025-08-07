c598ea3f0c61fdb288a1e8669edf3a3e5ad3fde3
Hussain-Bader <7ussain.3ali@gmail.com>
2025-08-08 00:08:41 +0300

    partcipant  teacher models,and config mongoose file

---
+++ models/Partcipant.js
@@ -0,0 +1,21 @@
+const { Schema } = require('mongoose')
+
+const participantSchema = new Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  passwordDigest: { type: String, required: true },
+  age: { type: Number, required: true },
+  image: { type: String, required: true },
+  currentCourses: { typey: Array, required: true },
+  previousCourses: { typey: Array, required: true },
+  Scheduel: [
+    { name: { type: String, required: true } },
+    { description: { type: String, required: true } },
+    { time_date: { type: String, required: true } },
+    { courses_id: { type: Schema.Types.ObjectId, ref: 'Course' } },
+    { place_id: { type: Schema.Types.ObjectId, ref: 'Provider' } }
+  ],
+  skills: { typey: Array, required: true }
+})
+
+module.exports = participantSchema
