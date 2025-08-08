4acd1d692d9692ce83994e78793e222f0c53f19b
Jawad <jawad.hassan.office@gmail.com>
2025-08-08 15:49:38 +0300

    finish updata skill

--- controllers/skillController.js
+++ controllers/skillController.js
@@ -1,4 +1,5 @@
 const { Skill } = require("../models")
+const { param } = require("../routes/skillRouter")
 
 const GetSkill = async (req, res) => {
   try {
@@ -22,10 +23,20 @@ const CreateSkill = async(req,res) => {
 }
 
 const UpdateSkill = async (req, res) => {
-  
+  try {
+    const skill = await Skill.findByIdAndUpdate(req.params.skill_id, req.body, {new:true})
+    res.status(200).send(skill)
+    
+  } catch (error) {
+    throw error
+    
+    
+  }
+
 }
 
 module.exports = {
   GetSkill,
-  CreateSkill
+  CreateSkill,
+  UpdateSkill
 }
--- routes/skillRouter.js
+++ routes/skillRouter.js
@@ -4,5 +4,6 @@ const controller = require('../controllers/skillController')
 router.get('/skill',controller.GetSkill)
 
 router.post('/skill',controller.CreateSkill)
+router.put('/skill/:skill_id',controller.UpdateSkill)
 
 module.exports = router
