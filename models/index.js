d49dc7eb95964f2d6c9da4b4ee20a1ce759e4c41
Jawad <jawad.hassan.office@gmail.com>
2025-08-08 14:45:51 +0300

    add index modlels, add skillSchema and providerSchema

---
+++ models/index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const userSchema = require('./User')
+const postSchema = require('./Post')
+
+const User = mongoose.model('User', userSchema)
+const Post = mongoose.model('Post', postSchema)
+
+module.exports = {
+  
+}
